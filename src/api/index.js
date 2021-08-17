a984fd48f19875ea68770a0ffad225c44a097992
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-08-17 15:50:04 +0100

    authentification added to thr front end route

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+
 export const fetchPosts = () => API.get('/post');
 export const createPost = (newPost) => API.post('/post', newPost);
 export const likePost = (id) => API.patch(`/post/${id}/likePost`);
