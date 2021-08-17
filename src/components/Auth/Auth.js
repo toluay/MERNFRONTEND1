13070808062c04162501ac5ebdf679917f413188
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-08-17 13:58:16 +0100

    Auth component added and also the action and reduce for the authentification with the google login plugin for google authentification on login page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -79,7 +79,7 @@ const SignUp = () => {
                 { isSignup ? 'Sign Up' : 'Sign In' }
               </Button>
               <GoogleLogin
-                clientId="564033717568-e5p23rhvcs4i6kffgsbci1d64r8hp6fn.apps.googleusercontent.com"
+                clientId="284393521690-cvknrfd1apafopdj827pf4u1ao4ouhv9.apps.googleusercontent.com"
                 render={(renderProps) => (
                   <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                     Google Sign In
