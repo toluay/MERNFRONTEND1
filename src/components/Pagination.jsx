9b3dd733846f6a6c9a596120b605abb29e2cab29
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-25 08:48:45 +0100

    pagenate componet edited to coreect eror

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 const Paginate = () => {
     const classes = useStyles();
+    
     return  (
         <Pagination
           classes={{ ul: classes.ul }}
