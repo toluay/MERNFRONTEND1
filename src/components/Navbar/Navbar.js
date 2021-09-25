454006442a403c73c419c255ee84b5219ec87eab
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-25 12:20:37 +0100

    nav component images added and replaced with the single  image used

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,8 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
+import memoriesLogo from '../../images/memoriesLogo.png';
+import memoriesText from '../../images/memoriesText.png';
 import memories from '../../images/memories.png';
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
@@ -39,10 +41,14 @@ const Navbar = () => {
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-          <div className={classes.brandContainer}>
-            <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+          {/* <div className={classes.brandContainer}> */}
+            {/* <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
             <img className={classes.image} src={memories} alt="icon" height="60" />
-          </div>
+          </div> */}
+          <Link to="/" className={classes.brandContainer}>
+        <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
+        <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+      </Link>
           <Toolbar className={classes.toolbar}>
             {user?.result ? (
               <div className={classes.profile}>
