36d03a1e133263f129ec58dfc9eead8a8b280871
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-26 22:24:00 +0100

    detailpost page created to display individual post

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react';
-import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core/';
-import { useDispatch, useSelector } from 'react-redux';
-import moment from 'moment';
-import { useParams, useHistory } from 'react-router-dom';
+import React  from 'react';
 
+// import useStyles from './styles';
 
 const Post = () => {
-
+<div>detaios</div>
 }
 export default Post;
