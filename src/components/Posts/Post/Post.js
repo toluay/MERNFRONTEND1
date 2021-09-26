2a659dd01f596506d72751eca7b2618462a03a90
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-26 23:01:47 +0100

    buttonbase imported and used , openpost function added to link to the detailed page of the post , e.stopPropagation() added to the onclick function in the button on the post component

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core/';
 
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import InfoIcon from '@material-ui/icons/Info';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
-import { deletePost, likePost } from '../../../actions/posts';
+import {getPost , deletePost, likePost } from '../../../actions/posts';
 
 
 import useStyles from './styles';
@@ -28,17 +29,29 @@ const Post = ({ post, setCurrentId }) => {
 
     return <><ThumbUpAltIcon fontSize="small" />&nbsp;Like</>;
   };
+  const openPost = (e) => {
+    history.push(`/posts/${post._id}`);
+  };
 
   return (
-    <Card className={classes.card} elevation={6}>
+    <Card className={classes.card} elevation={6} raised>
+        <ButtonBase
+        component="span"
+        name="test"
+        className={classes.cardAction}
+        onClick={openPost}
+      >
       <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
       {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-      <div className={classes.overlay2}>
-        <Button onClick={() => setCurrentId(post._id)} style={{ color: 'white' }} size="small">
+      <div className={classes.overlay2} name="edit">
+        <Button onClick={(e) =>{
+          e.stopPropagation();
+          setCurrentId(post._id);
+        } } style={{ color: 'white' }} size="small">
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
@@ -50,6 +63,7 @@ const Post = ({ post, setCurrentId }) => {
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
+      </ButtonBase>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
           <Likes />
