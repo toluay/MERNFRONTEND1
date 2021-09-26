f7335779304573c67bbf9a5bfa0630befac641f1
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-26 22:23:06 +0100

    useselector hook is used to get state value isloading and posts to condiction the loading of the post to the home componet

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,16 +1,31 @@
-import {  START_LOADING, END_LOADING, FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE} from '../constants/actionTypes';
+import {  START_LOADING, END_LOADING, FETCH_ALL,FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE} from '../constants/actionTypes';
 
 import * as api from '../api/index.js';
 
-export const getPosts = () => async (dispatch) => {
-    try {
-      const { data } = await api.fetchPosts();
-  
-      dispatch({ type: FETCH_ALL, payload: data });
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+
+export const getPosts = (page) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
+
+    dispatch({ type: FETCH_ALL, payload: { data, currentPage, numberOfPages } });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getPost = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+
+    const { data } = await api.fetchPost(id);
+
+    dispatch({ type: FETCH_POST, payload: { post: data } });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
